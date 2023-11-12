7d2c73d21e80fe44851e6a04d166a1084ff73953
doogunwo <dgw0601@naver.com>
2023-11-12 16:31:13 +0900

    타이블바 만듬

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.cookandroid.memo"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.cookandroid.memo"
@@ -32,7 +32,6 @@ android {
 }
 
 dependencies {
-
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
--- app/src/main/java/com/cookandroid/memo/MainActivity.java
+++ app/src/main/java/com/cookandroid/memo/MainActivity.java
@@ -1,8 +1,14 @@
 package com.cookandroid.memo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.PopupMenu;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.Window;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("내 메모");
+        //제목 변경
+
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int itemId = item.getItemId(); // 변수로 값을 받아옴
+        if(itemId==R.id.make){
+            createNewMemo();
+        }
+        if(itemId==R.id.info){
+            showDeveloperInfo();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    public void createNewMemo(){
+        Toast.makeText(this,"메모 작성 누름",Toast.LENGTH_SHORT).show();
+    }
+
+    public void showDeveloperInfo(){
+        Toast.makeText(this,"개발자 정보 누름",Toast.LENGTH_SHORT).show();
+    }
+
+
 }
